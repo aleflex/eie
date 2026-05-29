a73916b67f2ccb2e53e32e0a7964d26c9fffef2e
Aleflex <alejandrokaviraya@hotmail.com>
2026-05-28 22:04:07 -0400

    Fix syntax in courses component

--- frontend/src/app/components/courses/courses.component.ts
+++ frontend/src/app/components/courses/courses.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { CourseService, Course } from '../../services/course.service';
+import { CourseService, Curso as Course } from '../../services/course.service';
 import { RouterModule } from '@angular/router';
 
 @Component({
